4fa48948a8f7b1d9a0cf1d368b249f15f8baf6ce
Shizukanawa <deviousfinest@gmail.com>
2018-12-05 11:42:52 +0100

    Finished calculateDistanace function and calcrad

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-/* Point1: 57.043243, 9.917183 */
-/* Point2: 57.008360, 9.898382 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -57,6 +55,9 @@ Train moveTrain(Train *train, int station);
 double calculateDistance(double lat1, double long1, double lat2, double long2);
 double calculateRad(double degrees);
 
+/* Point1: 57.043243, 9.917183 */
+/* Point2: 57.008360, 9.898382 */
+
 int main(void)
 {
     Train IC4[AMOUNT_OF_TRAINS];
@@ -70,7 +71,7 @@ int main(void)
 
     IC4[0] = changeStatus(IC4[0], Down);
     printf("IC4: %d, Status: %d\n", 0, IC4[0].Status);
-    printf("%d to %lf", 1, calculateRad(1));
+    printf("Result: %lf\n", calculateDistance(57.043243, 9.917183, 57.008360, 9.898382));
     return EXIT_SUCCESS;
 }
 
@@ -92,8 +93,16 @@ Train moveTrain(Train *train, int station)
 
 double calculateDistance(double lat1, double long1, double lat2, double long2)
 {
-    double R = 6371000, Phi1, Phi2, DeltaPhi, DeltaLambda; /* Formular from https://www.movable-type.co.uk/scripts/latlong.html */
-    
+    double R = 6371000, a, c, Phi1, Phi2, DeltaPhi, DeltaLambda; /* Formular from https://www.movable-type.co.uk/scripts/latlong.html */
+    Phi1 = calculateRad(lat1);
+    Phi2 = calculateRad(lat2);
+    DeltaPhi = calculateRad(lat2 - lat1);
+    DeltaLambda = calculateRad(long2 - long1);
+    a = sin(DeltaPhi/2) * sin(DeltaPhi/2) +
+        cos(Phi1) * cos(Phi2) *
+        sin(DeltaLambda/2) * sin(DeltaLambda/2);
+    c = 2 * atan2(sqrt(a), sqrt(1-a));
+    return R * c;
 }
 
 double calculateRad(double degrees)
