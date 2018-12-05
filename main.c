ef6da604338e5010b58f98c530bb24c689bd8f97
knad123 <amad18@student.aau.dk>
2018-12-05 10:15:09 +0100

    Added Stations

--- main.c
+++ main.c
@@ -4,6 +4,37 @@
 const int AMOUNT_OF_STATIONS = 27;
 const int AMOUNT_OF_TRAINS = 10;
 
+enum Station
+{
+    Aalborg = 0,
+    Skalborg,
+    Svenstrup,
+    Støvring,
+    Skørping,
+    Arden,
+    Hobro,
+    Randers,
+    Langå,
+    Hadsten,
+    Århus,
+    Skanderborg,
+    Horsens,
+    Vejle,
+    Fredericia,
+    Middelfart,
+    Odense,
+    Langeskov,
+    Nyborg,
+    Korsør,
+    Slagelse,
+    Sorø,
+    Ringsted,
+    Roskilde,
+    Høje_Taastrup,
+    Valby,
+    København;
+}
+
 enum Status
 {
     Off,
@@ -23,7 +54,8 @@ int main(void)
 {
     Train IC4[AMOUNT_OF_TRAINS];
     int i;
-    for(i = 0; i < AMOUNT_OF_TRAINS; ++i)
+    Train *t;
+    for (i = 0; i < AMOUNT_OF_TRAINS; ++i)
     {
         IC4[i].Status = Off;
         printf("IC4: %d, Status: %d\n", i, IC4[i].Status);
