5d4ccabe26ee70f74323fda51d4e7730f9d66fe5
Shizukanawa <deviousfinest@gmail.com>
2018-12-05 10:09:32 +0100

    Fixed AMOUNT_OF_STATIONS

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define AMOUNT_OF_STATIONS 27
 const int AMOUNT_OF_STATIONS = 27;
 const int AMOUNT_OF_TRAINS = 10;
 
@@ -24,11 +23,13 @@ int main(void)
 {
     Train IC4[AMOUNT_OF_TRAINS];
     int i;
+
     for(i = 0; i < AMOUNT_OF_TRAINS; ++i)
     {
         IC4[i].Status = Off;
         printf("IC4: %d, Status: %d\n", i, IC4[i].Status);
     }
+    
     changeStatus(IC4[0], Down);
     printf("IC4: %d, Status: %d\n", 0, IC4[0].Status);
     return EXIT_SUCCESS;
